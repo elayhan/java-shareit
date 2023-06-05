1fa7390b6f0222a9e601d22400d2cd78ce1cb67e
e1ayhan <wwwelayhan@yandex.ru>
2023-06-05 22:40:24 +0300

    fix: style

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.coyote.Request;
-import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
